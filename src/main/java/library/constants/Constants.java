4708ff3f5111628b4b5be40f9efb0d9cee7debbc
Alexander MacLeod <30347591+AlexMacLeod23@users.noreply.github.com>
2018-08-16 22:58:50 +0100

    Added new constant, changed to primitive long

--- src/main/java/library/constants/Constants.java
+++ src/main/java/library/constants/Constants.java
@@ -2,7 +2,8 @@ package library.constants;
 
 public final class Constants {
 
-    public static final Long STANDARD_LOAN_PERIOD_WEEKS = 4L;
+    public static final long STANDARD_LOAN_PERIOD_WEEKS = 4;
+    public static final long AGE_OF_MINORITY = 16;
 
     private Constants() {}
 }
