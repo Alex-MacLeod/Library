0b272c5d1595d7da6881bf7d7d236f8d3d76ddb4
Alexander MacLeod <30347591+AlexMacLeod23@users.noreply.github.com>
2020-04-24 00:23:19 +0100

    Removed unused test import

--- src/test/java/library/customer/CustomerTest.java
+++ src/test/java/library/customer/CustomerTest.java
@@ -2,7 +2,6 @@ package library.customer;
 
 import library.item.Book;
 import library.item.Borrowable;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
